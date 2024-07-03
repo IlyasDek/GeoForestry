af87a1ee0b96fead175f1b0297cb5f03cb557f49
IlyasDek <ilyas.dekiyev@gmail.com>
2024-07-03 17:35:46 +0600

    Add JWT token validation endpoint and dependencies in AuthController

--- src/main/java/kz/eospatial/GeoForestry/controllers/AuthController.java
+++ src/main/java/kz/eospatial/GeoForestry/controllers/AuthController.java
@@ -4,15 +4,16 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import kz.eospatial.GeoForestry.config.SignInRequest;
+import kz.eospatial.GeoForestry.config.jwt.JwtService;
 import kz.eospatial.GeoForestry.services.AuthenticationService;
 import kz.eospatial.GeoForestry.config.jwt.JwtAuthenticationResponse;
+import kz.eospatial.GeoForestry.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -22,6 +23,8 @@ public class AuthController {
 
     private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
     private final AuthenticationService authenticationService;
+    private final JwtService jwtService;
+    private final UserService userService;
 
 //    @Operation(summary = "Регистрация пользователя")
 //    @PostMapping("/sign-up")
@@ -41,5 +44,21 @@ public class AuthController {
             logger.error("Error authorizing user {}: {}", request.getUsername(), e.toString()); // Detailed error logging
             throw e;
         }
+    }
+
+    @Operation(summary = "Validate JWT token")
+    @GetMapping("/validate-token")
+    public ResponseEntity<UserDetails> validateToken(@RequestParam String token) {
+        logger.info("Validating token");
+        String jwt = token.startsWith("Bearer ") ? token.substring(7) : token;
+        try {
+            if (jwtService.isTokenValid(jwt, userService.loadUserByUsername(jwtService.extractUserName(jwt)))) {
+                UserDetails userDetails = userService.loadUserByUsername(jwtService.extractUserName(jwt));
+                return ResponseEntity.ok(userDetails);
+            }
+        } catch (Exception e) {
+            logger.error("Error validating token: {}", e.toString());
+        }
+        return ResponseEntity.status(401).build();
     }
 }
