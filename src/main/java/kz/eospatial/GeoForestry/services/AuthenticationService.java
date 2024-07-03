3694ad918ed411523025e75e1048ae0bab147a67
IlyasDek <ilyas.dekiyev@gmail.com>
2024-07-03 17:36:21 +0600

    Update authenticate method to include userId in JwtAuthenticationResponse

--- src/main/java/kz/eospatial/GeoForestry/services/AuthenticationService.java
+++ src/main/java/kz/eospatial/GeoForestry/services/AuthenticationService.java
@@ -4,12 +4,14 @@ import kz.eospatial.GeoForestry.config.SignInRequest;
 import kz.eospatial.GeoForestry.config.jwt.JwtAuthenticationResponse;
 import kz.eospatial.GeoForestry.config.jwt.JwtService;
 import kz.eospatial.GeoForestry.user.UserService;
+import kz.eospatial.GeoForestry.user.Users;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -50,7 +52,9 @@ public class AuthenticationService {
         var userDetails = loadUserDetails(request.getUsername());
         var jwt = jwtService.generateToken(userDetails);
 
-        return new JwtAuthenticationResponse(jwt);
+        Users user = userService.findByUsername(request.getUsername());
+
+        return new JwtAuthenticationResponse(jwt, user.getId());
     }
 
     private void authenticate(String username, String password) {
